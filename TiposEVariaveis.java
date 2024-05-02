db07459cee05d747ec7188fc9c629c96bcc263f6
Caio Varago <varago079@gmail.com>
2024-05-02 17:13:23 -0300

    Projeto sobre Tipos E Variaveis (Melhorado)

--- TiposEVariaveis.java
+++ TiposEVariaveis.java
@@ -1,5 +1,24 @@
 public class TiposEVariaveis {
     public static void main (String [] args) {
-        System.out.print("I need to learn this fast!");
+        int umPouco = 200000000;
+        long doisMuito = 255665655L;
+        double outroNumero = 2.5;
+        final float NUMERO_RACIONAL = 6.4F;
+        byte bitOito= 8;
+        short pequenoNum = 24;
+
+        System.out.println(doisMuito - umPouco);
+        System.out.println(outroNumero + NUMERO_RACIONAL);
+        System.out.println(pequenoNum / bitOito);
+
+        outroNumero = 4.6;
+
+        System.out.println(outroNumero + NUMERO_RACIONAL);
+
+        String meuNome = "Caio";
+        char darNota = 'B';
+        boolean alunoMedio = true;
+
+        System.out.println(meuNome + " - " + "Aluno Nota: " + darNota + " - " + alunoMedio);
     }
 }
