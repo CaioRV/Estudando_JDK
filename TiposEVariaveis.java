7896897adebf31e10d353f9f3d67b52040c5401d
Caio Varago <varago079@gmail.com>
2024-05-02 11:36:12 -0300

    Criando o 1° projeto de exemplo de TiposEVariaveis

--- TiposEVariaveis.java
+++ TiposEVariaveis.java
@@ -1,5 +1,5 @@
 public class TiposEVariaveis {
     public static void main (String [] args) {
-        System.out.print("Hello World");
+        System.out.print("I need to learn this fast!");
     }
 }
---
+++ out/production/Estudando_JDK/README.md
@@ -0,0 +1,2 @@
+# Estudando_JDK
+Projetos para o propósito de estudar/aprender a linguagem de programação Java.
